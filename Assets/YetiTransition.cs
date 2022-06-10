a231579bbbcdb1006230dc53e291db4dee59080f
Brandon <brandon.thompson@bellevuecollege.edu>
2022-06-10 11:27:34 -0700

    Added trigger transition to yeti cave
    After door opens player can move into yeti cave section of the game.

---
+++ Assets/YetiTransition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YetiTransition : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(2);
+    }
+}
